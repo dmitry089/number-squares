07a37d69d9672202d1b60ce0f2de4814f51e06a7
“dmitry089” <dmitry.karaulov@rambler.ru>
2023-01-27 23:43:54 +0400

    module 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import ru.netology.sqr.NumberSquares;
 public class Main {
     public static void main(String[] args) {
         NumberSquares service = new NumberSquares();
-        int result = service.calcSqrt(1234, 5678);
+        int result = service.calcSqrt(200, 300);
         System.out.println("Количество квадратов : " + result);
     }
 }
